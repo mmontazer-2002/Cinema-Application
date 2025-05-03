4c633f50bfa6f6105e3f44125bd4ace32747ccdd
mahdie montazeri <mmontazer1831@gmail.com>
2025-05-03 14:14:21 +0330

    feat:adding the theater class.

---
+++ src/main/java/com/app/entities/Theater.java
@@ -0,0 +1,46 @@
+package com.app.entities;
+
+public class Theater {
+    private int theaterId;
+    private int totalSeats;
+    private int bookedSeats = 0;
+
+    public Theater(int theaterId, int totalSeats) {
+        this.theaterId = theaterId;
+        this.totalSeats = totalSeats;
+    }
+
+    public int getTheaterId() {
+        return theaterId;
+    }
+
+    public void setTheaterId(int theaterId) {
+        this.theaterId = theaterId;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public int getBookedSeats() {
+        return bookedSeats;
+    }
+
+    public void setBookedSeats(int bookedSeats) {
+        this.bookedSeats = bookedSeats;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Theater{" +
+                "theaterId=" + theaterId +
+                ", totalSeats=" + totalSeats +
+                ", bookedSeats=" + bookedSeats +
+                '}';
+    }
+}
