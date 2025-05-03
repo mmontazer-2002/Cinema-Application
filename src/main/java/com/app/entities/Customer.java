92516494e8e58a7bbab6b903d53738af6f661f1a
mahdie montazeri <mmontazer1831@gmail.com>
2025-05-03 12:07:02 +0330

    feat:adding the customer class

--- src/main/java/com/app/entities/Customer.java
+++ src/main/java/com/app/entities/Customer.java
@@ -1,4 +1,46 @@
 package com.app.entities;
 
 public class Customer {
+    private int customerId;
+    private String name;
+    private String surname;
+
+    public Customer(int customerId, String name, String surname) {
+        this.customerId = customerId;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
